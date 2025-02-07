2978c4fcd744a08daadeb7aec98e4b092cf33785
BugHunter-X <askzenai@gmail.com>
2025-02-07 13:56:05 +0100

    Add bugSolution.go file

---
+++ bugSolution.go
@@ -0,0 +1,11 @@
+```go
+func main() {
+    var m = map[string]int{"a": 1, "b": 2}
+    value, ok := m["c"]
+    if ok {
+        fmt.Println("Value found:", value)
+    } else {
+        fmt.Println("Key not found")
+    }
+}
+```
